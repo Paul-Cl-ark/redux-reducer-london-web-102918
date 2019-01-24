aa84f5774b96230da09200f6a6303008ea8452a5
Paul-Cl-ark <clark.paul123@gmail.com>
2019-01-24 21:58:48 +0000

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends (state = { friends: [] }, action) {
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return {...state, friends: [...state.friends, action.friend]}
+    case 'REMOVE_FRIEND':
+      const indexOfFriendToRemove = state.friends.indexOf(action.id)
+      const updatedFriends = [...state.friends]
+      updatedFriends.splice(indexOfFriendToRemove -1, 1)
+      return { ...state, friends: updatedFriends }
+    default:
+      return state
+  }
 }
